f2fe1647b0c2dcaf3fbf0c31bff89b3b36a4474e
MiguelMGdev <miguelguillen24mg@gmail.com>
2024-03-17 13:26:44 -0500

    Limpieza

--- server/database/database.js
+++
@@ -1,8 +0,0 @@
-const {Sequelize} = require('sequelize');
-
-const sequelize = new Sequelize('retrogamer_db','postgres','Progresql24',{
-    host: 'localhost',
-    dialect: 'postgres'
-})
-
-module.exports = sequelize;

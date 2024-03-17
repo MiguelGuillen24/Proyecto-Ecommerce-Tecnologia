7e06dee5c80c85192dfe794faf2fae0d97b84a5f
MiguelMGdev <miguelguillen24mg@gmail.com>
2024-03-17 13:26:18 -0500

    Ingreso de php

---
+++ index.php
@@ -0,0 +1,109 @@
+<?php
+ require_once './models/components/social_network.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Retrogamer</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    <h2>Curso php</h2>
+
+    <?php echo "Lubutech" ?>
+
+    <?php
+
+    print "<h4>título h4</h4>";
+    echo "<hr>";
+    ?>
+
+    <?php
+
+    print "título h4";
+
+    ?>
+
+    <h2 style="color:blue">texto azul</h2>
+
+    <h2 <?php echo 'style="color:blue"' ?>>texto azul 2</h2>
+
+
+    <footer>
+    <table class="table table-borderless">
+  <thead>
+    <tr>
+      <th scope="col">Sobre Nosotros</th>
+      <th scope="col">Nuestras Redes</th>
+      <th scope="col"><img src></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Misión y Visión</th>
+      <td><?php  social_res("x"); ?></td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+    </footer>
+</body>
+
+</html>
---
+++ models/components/social_network.php
@@ -0,0 +1,20 @@
+<?php
+
+function social_res($social)
+{
+    try {
+        switch ($social) {
+            case "x":
+                $url = "https://cdn-icons-png.flaticon.com/512/5969/5969020.png";
+                break;
+                // Agrega más casos según necesites para otras redes sociales
+            default:
+                $url = "#";
+                break;
+        }
+        // Imprime el icono
+        echo "<img src='$url'>";
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
